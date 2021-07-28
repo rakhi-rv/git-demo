ad6da35b74d660f1437e1531441c50da276e8e63
Rakhi Vishwakarma <rakhi.vishwakarma@tradeshift.com>
2021-07-28 20:05:43 +0200

    fixup! Added one more commit

--- src/main/java/com/git/demo/Main.java
+++ src/main/java/com/git/demo/Main.java
@@ -3,6 +3,6 @@ package com.git.demo;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test git commands - squash");
+        System.out.println("Test git commands - fixup");
     }
 }
